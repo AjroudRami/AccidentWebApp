b02e46d8191b40183865a80ec9adaaf8d911a6f4
scipio3000 <gunther@gameslabs.net>
2018-02-18 22:36:39 +0100

    feat: add marker icons

--- client/src/components/maps/Map.vue
+++ client/src/components/maps/Map.vue
@@ -5,11 +5,14 @@
 <template>
   <div>
     <gmap-map
-      :center="{lat:43.615552, lng: 7.071875}"
+      :center="{lat: location.lat, lng: location.lng}"
       :zoom="17"
       :style="{height: height + 'px', width: '100%'}"
+      @rightclick='moveLocation($event)'
     >
-    <gmap-marker v-for="item in items" :position="item">
+    <gmap-marker :position="location" icon="/src/images/car.png">
+    </gmap-marker>
+    <gmap-marker v-for="item in items" :position="item" icon="/src/images/crash.png">
     </gmap-marker>
     </gmap-map>
   </div>
@@ -17,10 +20,12 @@
 
 <script>
 import api from '../../services/api'
+import location from '../../services/location'
 
 export default {
   data: function () {
     return {
+      location: location,
       items: [
         {lat:43.615552, lng: 7.071875},
         {lat:43.615452, lng: 7.071875}
@@ -28,10 +33,20 @@ export default {
       height: window.innerHeight - 57
     }
   },
+  methods: {
+    moveLocation: function($event) {
+      this.location.setLocation($event.latLng.lat(), $event.latLng.lng())
+    }
+  },
   mounted: function () {
-    api.get('/accidents').then(res => {
-      console.log(res.data)
-    }).catch(console.error)
+    this.interval = setInterval(() => {
+      api.get('/accidents?lat=10&lng=10&radius=1').then(res => {
+        console.log(res.data)
+      }).catch(console.error)
+    }, 5000)
+  },
+  beforeDestroy: function () {
+    clearInterval(this.interval)
   }
 }
 </script>
---
+++ client/src/services/location.js
@@ -0,0 +1,12 @@
+const location = {
+  lat: 43.615552,
+  lng: 7.071875
+}
+export default {
+  lat: location.lat,
+  lng: location.lng,
+  setLocation: function(lat, lng) {
+    this.lat = lat
+    this.lng = lng
+  }
+}
