7f2f0a1dade606b853f284004759c839e7b033f5
scipio3000 <gunther@gameslabs.net>
2018-02-18 22:11:30 +0100

    feat: client files

---
+++ client/src/components/maps/Map.vue
@@ -0,0 +1,23 @@
+<style scoped>
+
+</style>
+
+<template>
+  <div>
+    <gmap-map
+      :center="{lat:43.615552, lng: 7.071875}"
+      :zoom="17"
+      :style="{height: height + 'px', width: '100%'}"
+    ></gmap-map>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      height: window.innerHeight - 57
+    }
+  }
+}
+</script>
---
+++ client/src/components/navigation/Top.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-navbar toggleable="md">
+
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+    <b-navbar-brand href="#">Ma carte</b-navbar-brand>
+
+    <b-collapse is-nav id="nav_collapse">
+
+      <b-navbar-nav>
+        <b-nav-item>Accidents</b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-item-dropdown right>
+          <template slot="button-content">
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profil</b-dropdown-item>
+          <b-dropdown-item href="#">Se déconnecter</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+
+    </b-collapse>
+  </b-navbar>
+</template>
