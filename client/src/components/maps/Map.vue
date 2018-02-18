0092795a29ac2cf593af19072e873f842f2fa7eb
scipio3000 <gunther@gameslabs.net>
2018-02-18 22:18:43 +0100

    feat: add api service

--- client/src/components/maps/Map.vue
+++ client/src/components/maps/Map.vue
@@ -8,16 +8,30 @@
       :center="{lat:43.615552, lng: 7.071875}"
       :zoom="17"
       :style="{height: height + 'px', width: '100%'}"
-    ></gmap-map>
+    >
+    <gmap-marker v-for="item in items" :position="item">
+    </gmap-marker>
+    </gmap-map>
   </div>
 </template>
 
 <script>
+import api from '../../services/api'
+
 export default {
   data: function () {
     return {
+      items: [
+        {lat:43.615552, lng: 7.071875},
+        {lat:43.615452, lng: 7.071875}
+      ],
       height: window.innerHeight - 57
     }
+  },
+  mounted: function () {
+    api.get('/accidents').then(res => {
+      console.log(res.data)
+    }).catch(console.error)
   }
 }
 </script>
