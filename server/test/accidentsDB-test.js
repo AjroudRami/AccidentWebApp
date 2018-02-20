ac1e431e543857f0a79c2937cfd3f4e62b459d8f
Ajroud Rami <ajroud.rami@hotmail.fr>
2018-02-20 18:56:39 +0100

    tests passing

--- server/test/accidentsDB-test.js
+++ server/test/accidentsDB-test.js
@@ -3,7 +3,8 @@ process.env.NODE_ENV = 'test';
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var mongoose = require("mongoose");
-
+Promise = require('promise');
+mongoose.Promise = Promise;
 var server = require('../app');
 var Accident = require("../database/model/AccidentSchema");
 
@@ -13,9 +14,22 @@ chai.use(chaiHttp);
 describe('Accidents tests', function () {
 
     console.log('started');
-    Accident.collection.drop();
-    beforeEach(function(done) {
+    var newAccident;
+    //Drop collection before all test once
+    before(function (done) {
+        console.log('before');
+        Accident.collection.remove();
+        newAccident = new Accident({
+            loc: [45.6237776, 7.1473906],
+            placeName: "Route des Prolines",
+            seriousness: 3
+            // date: Date.now()
+        });
 
+        newAccident.save().then(done());
+    });
+
+    /**beforeEach( function(done) {
         var newAccident = new Accident({
             loc: [45.6237776, 7.1473906],
             placeName: "Route des Prolines",
@@ -26,52 +40,43 @@ describe('Accidents tests', function () {
         newAccident.save(function(err) {
             done();
         });
+        console.log('before each')
 
-    });
-
-    afterEach(function(done) {
-        Accident.collection.drop();
-        done();
-    });
+    });**/
 
     describe('GET api/accidents/:AccidentID', function () {
         it('it should GET a accident by the given id', function (done) {
-            var newAccident = new Accident({
-                loc: [43.6237776, 7.0473906],
-                placeName: "Route des Dolines",
-                seriousness: 5
-                // date: Date.now()
-            });
+            var accident = {
+                loc: [44.6237776, 8.0473906],
+                placeName: "Route du Soleil",
+                seriousness: 2
+            };
 
-            newAccident.save(function (err, newAccident) {
-                chai.request(server)
-                    .get('/api/accidents/' + newAccident.id)
-                    .send(newAccident)
-                    .end(function (err, res) {
-                        res.should.have.status(200);
+            chai.request(server)
+                .get('/api/accidents/' + newAccident._id)
+                .send(newAccident)
+                .end(function (err, res) {
+                    res.should.have.status(200);
 
-                        res.should.be.json;
-                        res.body.should.be.a('object');
+                    res.should.be.json;
+                    res.body.should.be.a('object');
 
-                        res.body.should.have.property('_id');
-                        res.body.should.have.property('placeName');
-                        res.body.should.have.property('date');
-                        res.body.should.have.property('loc');
-                        res.body.should.have.property('seriousness');
+                    res.body.should.have.property('_id');
+                    res.body.should.have.property('placeName');
+                    res.body.should.have.property('date');
+                    res.body.should.have.property('loc');
+                    res.body.should.have.property('seriousness');
 
-                        res.body._id.should.equal(newAccident.id);
-                        res.body.loc[0].should.equal(43.6237776);
-                        res.body.loc[1].should.equal(7.0473906);
-                        res.body.placeName.should.equal('Route des Dolines');
-                        res.body.seriousness.should.equal(5);
+                    res.body._id.should.equal('' + newAccident._id);
+                    res.body.loc[0].should.equal(newAccident.loc[0]);
+                    res.body.loc[1].should.equal(newAccident.loc[1]);
+                    res.body.placeName.should.equal(newAccident.placeName);
+                    res.body.seriousness.should.equal(newAccident.seriousness);
 
-                        done();
-                    });
-            });
+                    done();
+                });
         });
     });
-
-
 
     describe('POST api/accidents/', function() {
         it('it should add a new accident to the database', function(done) {
@@ -90,13 +95,12 @@ describe('Accidents tests', function () {
                     res.should.be.json;
                     res.body.should.be.a('object');
 
-                    res.body.should.have.property('_id');
+                    res.body.should.have.property('id');
                     res.body.should.have.property('placeName');
                     res.body.should.have.property('date');
                     res.body.should.have.property('loc');
                     res.body.should.have.property('seriousness');
 
-                    res.body._id.should.equal(accident.id);
                     res.body.loc[0].should.equal(accident.loc[0]);
                     res.body.loc[1].should.equal(accident.loc[1]);
                     res.body.placeName.should.equal(accident.placeName);
@@ -104,31 +108,23 @@ describe('Accidents tests', function () {
 
                     done();
                 })
-            });
+        });
     });
-
 
     describe('GET api/accidents/', function() {
         it('it should return at least one accident', function(done) {
-
-            var lon = 45.6237776;
-            var lat = 7.1473906;
-            var rad = 1;
-
             chai.request(server)
-                .get('/api/accidents?lat=' + lat + '&lon=' + lon + '&rad=' + rad)
-                .send()
-                .end(function(err, res) {
+                .get('/api/accidents/?lat=' + 7.1473906 + '&lon=' + 45.6237776 + '&radius=1000')
+                .send({lon: 45.6237776, lat: 7.1473906, radius: 1000})
+                .end(function (err, res) {
                     res.should.have.status(200);
 
                     res.should.be.json;
-                    res.body.should.be.a('object');
-
-                    expect(res.body).to.have.lengthOf(1);
-
-                    done(err);
-                })
+                    res.body.should.be.a('array');
+                    done();
+                });
         });
-    })
+    });
 });
+
 
